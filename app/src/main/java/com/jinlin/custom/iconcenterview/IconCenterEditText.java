4f9947df4536d5b29c9382263203fcc719959def
J!nl!n <2015-249-pc@2015-249deMacBook-Pro.local>
2015-06-07 15:32:37 +0800

    Initial commit

---
+++ app/src/main/java/com/jinlin/custom/iconcenterview/IconCenterEditText.java
@@ -0,0 +1,103 @@
+package com.jinlin.custom.iconcenterview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+/**
+ * Created by J!nl!n on 2015/1/26.
+ */
+public class IconCenterEditText extends EditText implements View.OnFocusChangeListener, View.OnKeyListener {
+    private static final String TAG = IconCenterEditText.class.getSimpleName();
+    /**
+     * 是否是默认图标再左边的样式
+     */
+    private boolean isLeft = false;
+    /**
+     * 是否点击软键盘搜索
+     */
+    private boolean pressSearch = false;
+    /**
+     * 软键盘搜索键监听
+     */
+    private OnSearchClickListener listener;
+
+    public void setOnSearchClickListener(OnSearchClickListener listener) {
+        this.listener = listener;
+    }
+
+    public IconCenterEditText(Context context) {
+        this(context, null);
+        init();
+    }
+
+    public IconCenterEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.editTextStyle);
+        init();
+    }
+
+    public IconCenterEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setOnFocusChangeListener(this);
+        setOnKeyListener(this);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (isLeft) { // 如果是默认样式，则直接绘制
+            super.onDraw(canvas);
+        } else { // 如果不是默认样式，需要将图标绘制在中间
+            Drawable[] drawables = getCompoundDrawables();
+            if (drawables != null) {
+                Drawable drawableLeft = drawables[0];
+                if (drawableLeft != null) {
+                    float textWidth = getPaint().measureText(getHint().toString());
+                    int drawablePadding = getCompoundDrawablePadding();
+                    int drawableWidth = drawableLeft.getIntrinsicWidth();
+                    float bodyWidth = textWidth + drawableWidth + drawablePadding;
+                    canvas.translate((getWidth() - bodyWidth - getPaddingLeft() - getPaddingRight()) / 2, 0);
+                }
+            }
+            super.onDraw(canvas);
+        }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        Log.d(TAG, "onFocusChange execute");
+        // 恢复EditText默认的样式
+        if (!pressSearch && TextUtils.isEmpty(getText().toString())) {
+            isLeft = hasFocus;
+        }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        pressSearch = (keyCode == KeyEvent.KEYCODE_ENTER);
+        if (pressSearch && listener != null) {
+            /*隐藏软键盘*/
+            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm.isActive()) {
+                imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+            }
+            listener.onSearchClick(v);
+        }
+        return false;
+    }
+
+    public interface OnSearchClickListener {
+        void onSearchClick(View view);
+    }
+
+}
---
+++ app/src/main/java/com/jinlin/custom/iconcenterview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jinlin.custom.iconcenterview;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity{
+    private EditText et_search;
+
+    private IconCenterEditText icet_search;
+
+    @Override
+    public void iniView() {
+        setContentView(R.layout.activity_main);
+
+        et_search = (EditText) findViewById(R.id.et_search);
+        icet_search = (IconCenterEditText) findViewById(R.id.icet_search);
+
+        // 实现TextWatcher监听即可
+        icet_search.setOnSearchClickListener(new IconCenterEditText.OnSearchClickListener() {
+            @Override
+            public void onSearchClick(View view) {
+                Toast.makeText(MainActivity.this, "i'm going to seach", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
